3876074ddc7c2bcb9d221ece3e9e72eece7d7a13
Chloe <gtalk.with.nani@gmail.com>
2023-09-27 22:20:05 +0900

    GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,11 +14,7 @@ export default function Document() {
         <script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });`,
           }}
         />
       </Head>
