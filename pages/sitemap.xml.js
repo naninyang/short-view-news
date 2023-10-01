4b6d256b3919e4e6272e6253136cbfda2492f262
Chloe <gtalk.with.nani@gmail.com>
2023-10-01 22:27:31 +0900

    Change the last mod format of the sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,13 @@
 const API = 'https://news.dev1stud.io/api/sitemapData';
 
+function formatDate(dateString) {
+  const dateArray = dateString.split(' ').map((part) => part.replace(/[^0-9]/g, ''));
+  const year = dateArray[0];
+  const month = dateArray[2].padStart(2, '0');
+  const day = dateArray[4].padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
 function generateSiteMap(shorts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -11,7 +19,7 @@ function generateSiteMap(shorts) {
           return `
             <url>
               <loc>https://news.dev1stud.io/news/${idx}</loc>
-              <lastmod>${created}</lastmod>
+              <lastmod>${formatDate(created)}</lastmod>
             </url>
           `;
         })
