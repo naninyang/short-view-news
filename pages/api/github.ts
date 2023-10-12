c58b21e140be84d60eaf9df3590cc165234a62c4
Chloe <gtalk.with.nani@gmail.com>
2023-10-12 17:44:51 +0900

    Update github.ts

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -26,7 +26,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
     res.status(200).send(response.data);
   } catch (error) {
-    console.error('에러 발생:', error.message);
+    console.error('에러 발생:', (error as any).message);
     res.status(500).send('Failed to fetch data from GitHub');
   }
 };
