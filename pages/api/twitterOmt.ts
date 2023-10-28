14be856975f0764fd50b4c506b830556adad0273
Chloe <gtalk.with.nani@gmail.com>
2023-10-28 22:53:51 +0900

    Twitter OMT no res bug fixed

--- pages/api/twitterOmt.ts
+++ pages/api/twitterOmt.ts
@@ -39,7 +39,8 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
     const mdFiles = treeResponse.data.tree
       .filter(
-        (file: any) => file.path.startsWith(`src/pages/twitter-${process.env.NODE_ENV}`) && file.path.endsWith('.md'),
+        (file: any) =>
+          file.path.startsWith(`src/pages/twitter-omt-${process.env.NODE_ENV}`) && file.path.endsWith('.md'),
       )
       .sort((a: any, b: any) => b.path.localeCompare(a.path))
       .slice(start, start + count);
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,12 @@ self.addEventListener('fetch', (event) => {
   if (
     event.request.url.includes('/api/pages') ||
     event.request.url.includes('/api/sheets') ||
-    event.request.url.includes('/api/articles') ||
-    event.request.url.includes('/api/periodt') ||
+    event.request.url.includes('/api/articlesNews') ||
+    event.request.url.includes('/api/articlesEntertainment') ||
+    event.request.url.includes('/api/periodtOmt') ||
+    event.request.url.includes('/api/periodtTimeline') ||
+    event.request.url.includes('/api/historyNaver') ||
+    event.request.url.includes('/api/historyYouTube') ||
     event.request.url.includes('https://naver-news-opengraph.vercel.app/api/og')
   ) {
     event.respondWith(
